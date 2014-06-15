f4319068628114fb32ea185c3ebb83ec6d0f7d99
Scott Daniels <scott.daniels@proquest.com>
2014-06-15 14:03:43 -0400

    fixed some styles for section title and site log. added marker to google map

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,30 @@
-console.log('\'Allo \'Allo!');
+// google maps code
+function initialize() {
+    var myLatlng = new google.maps.LatLng(42.241624, -83.588372),
+        map_canvas = document.getElementById("map"),
+        map_options = {
+            center: myLatlng,
+            zoom: 12,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        },
+        map = new google.maps.Map(map_canvas, map_options),
+        marker = new google.maps.Marker({
+            position: myLatlng,
+            map: map
+        });;
+}
+google.maps.event.addDomListener(window, 'load', initialize);
+
+(function ($) {
+    "use strict";
+
+    $(".nav-link").click(function (e) {
+        e.preventDefault();
+
+        var loc = e.target.href.split("#")[1];
+        $('html, body').animate({
+            scrollTop: $("#" + loc).offset().top - 20
+        }, 1200);
+    });
+
+})($);
